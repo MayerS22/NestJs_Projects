eba778ac79245d04a1e343980a4f75f4f981cffb
mayers22 <mayersoliman4@gmail.com>
2025-07-08 13:47:29 +0300

    confugration system

--- Authentication/src/app.module.ts
+++ Authentication/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module ,ValidationPipe,MiddlewareConsumer} from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule ,ConfigService} from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,11 +11,18 @@ import { Report } from './reports/reports.entity';
 import cookieSession from 'cookie-session';
 
 @Module({
-  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
-    type:'sqlite',
-    database:'db.sqlite',
-    entities:[User,Report],
-    synchronize:true,
+  imports: [UsersModule, ReportsModule, ConfigModule.forRoot({
+    isGlobal:true,
+    envFilePath:`.env.${process.env.NODE_ENV}`,
+  }), 
+  TypeOrmModule.forRootAsync({
+    inject:[ConfigService],
+    useFactory:(configService:ConfigService)=>({
+      type:'sqlite',
+      database:configService.get<string>('DB_NAME'),
+      entities:[User,Report],
+      synchronize:true,
+    }),
   })],
   controllers: [AppController],
   providers: [AppService, {
--- Authentication/test/auth.e2e-spec.ts
+++ Authentication/test/auth.e2e-spec.ts
@@ -29,4 +29,29 @@ describe('Authentication System (e2e)', () => {
       })
       
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = "test12345@test.com";
+    const password = "123456";
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({email, password})
+      .expect(201)
+      .catch(err => {
+        console.log('Signup error:', err.response.body);
+        throw err;
+      });
+
+    const cookie = res.get('Set-Cookie');
+    expect(cookie).toBeDefined();
+
+    const cookieHeader = Array.isArray(cookie) ? cookie[0] : cookie;
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookieHeader || '')
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
