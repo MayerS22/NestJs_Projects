a65ca3abc65c1aab98ba625476a43275ae6bea56
MayerS22 <mayersoliman4@gamil.com>
2025-07-09 13:02:01 +0300

    Relations with TypeORM part 1

---
+++ Authentication/src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,28 @@
+import { IsString, IsNumber } from "class-validator";
+
+export class CreateReportDto{
+    
+
+    @IsNumber()
+    price:number;
+
+
+    @IsString()
+    make:string;
+
+
+    @IsString()
+    model:string;
+
+    @IsNumber()
+    year:number;
+    
+    @IsNumber()
+    lng:number;
+
+    @IsNumber()
+    lat:number;
+
+    @IsNumber()
+    mileage:number;
+}
